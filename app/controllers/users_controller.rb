775400d4008f226ad0cddf6b5d2ea933dd2befcb
Geirskogul <marie.efimova@gmail.com>
2012-04-09 15:50:59 +0400

    Editing users by admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_filter :authenticate, :only => [:edit, :update]
-    before_filter :correct_user, :only => [:edit, :update]
-    before_filter :admin_user,   :only => [:destroy, :new, :create]
+    #  before_filter :correct_user, :only => [:edit, :update]
+    before_filter :admin_user,   :only => [:destroy, :new, :create, :edit, :update]
     def index
         @title = "All users"
         @users = User.paginate(:page => params[:page])
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     end
   def new
       @user = User.new
-      @title = "Sign up"
+      @title = "New user"
   end
     def create
         @user = User.new(params[:user])
@@ -21,12 +21,12 @@ class UsersController < ApplicationController
             #  flash[:success] = "Welcome to the Courier Tracking App!"
             redirect_to users_path
             else
-            @title = "Sign up"
+            @title = "New user"
             render 'new'
         end
     end
     def edit
-        #   @user = User.find(params[:id])
+        @user = User.find(params[:id])
         @title = "Edit user"
     end
     def update
